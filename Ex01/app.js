7e29f63d56421009f87499ff7f178c8ac0b3dfca
Igor Minakawa Muraoka <igormuraoka97@gmail.com>
2022-10-23 13:09:41 -0300

    Ajusta Ex01

--- Ex01/app.js
+++ Ex01/app.js
@@ -1,22 +1,21 @@
-function procuraMinMax([...valores]){
-    let min = Infinity;
-    let max = -999;
-    valores.forEach(n => {
-        if (n > max){
-            max = n;
-        } else if (n < min){
-            min = n;
-        }
-    })
-    if(max == -999){
-        max = 'Não foi possível encontrar'
-    }
-    if(min == Infinity){
-        min = 'Não foi possível encontrar'
+function procuraMinMax(valores){
+    if(!Array.isArray(valores) || !valores.length){
+        console.log('Não é possível encontrar');
+    } else {
+        let max = valores[0];
+        let min = valores[0];
+
+        valores.forEach(n => {
+            if (n > max){
+                max = n;
+            } else if (n < min){
+                min = n;
+            }
+        })
+
+        console.log({ min, max });
     }
 
-    return console.log(`Maior valor: ${max}
-Menor valor: ${min}`);
 }
 
 procuraMinMax([56, 65, 64, -3, 23, 42, -15, 65, 14, 32, 78, 70, 47]);
